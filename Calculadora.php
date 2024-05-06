36e9c5863f421a0ae2f7e6cf978c31f93e9d27f0
CaioLabes <caiolabesdecastro@gmail.com>
2024-05-06 19:45:38 -0300

    Commit 2

--- Calculadora.php
+++ Calculadora.php
@@ -17,33 +17,35 @@
             <input type="number" id="num1" name="num1" value="<?php if(isset($_POST['num1'])) echo $_POST['num1']; ?>" ><br><br>
             <label for="op">Operador:</label><br>
             <select id="op" name="op">
-                <option value="+" <?php if(isset($_POST['op']) && $_POST['op'] == '+') echo 'selected'; ?>>+</option>
-                <option value="-" <?php if(isset($_POST['op']) && $_POST['op'] == '-') echo 'selected'; ?>>-</option>
-                <option value="*" <?php if(isset($_POST['op']) && $_POST['op'] == '*') echo 'selected'; ?>>*</option>
-                <option value="/" <?php if(isset($_POST['op']) && $_POST['op'] == '/') echo 'selected'; ?>>/</option>
-                <option value="^" <?php if(isset($_POST['op']) && $_POST['op'] == '^') echo 'selected'; ?>>^</option>
-                <option value="n!" <?php if(isset($_POST['op']) && $_POST['op'] == 'n!') echo 'selected'; ?>>n!</option>
+            <option value="+" <?php if(isset($_POST['op']) && $_POST['op'] == '+') echo 'selected'; ?>>+</option>
+            <option value="-" <?php if(isset($_POST['op']) && $_POST['op'] == '-') echo 'selected'; ?>>-</option>
+            <option value="*" <?php if(isset($_POST['op']) && $_POST['op'] == '*') echo 'selected'; ?>>*</option>
+            <option value="/" <?php if(isset($_POST['op']) && $_POST['op'] == '/') echo 'selected'; ?>>/</option>
+            <option value="^" <?php if(isset($_POST['op']) && $_POST['op'] == '^') echo 'selected'; ?>>^</option>
+            <option value="n!" <?php if(isset($_POST['op']) && $_POST['op'] == 'n!') echo 'selected'; ?>>n!</option>
             </select><br><br>
             <label for="num2">Número 2:</label><br>
             <input type="number" id="num2" name="num2" value="<?php if(isset($_POST['num2'])) echo $_POST['num2']; ?>" ><br><br>
+            <label for="resultado">Resultado:</label><br>
+            <input type="text" id="resultado" name="resultado" value="<?php if(isset($result)) echo $result; ?>" readonly><br><br>
             <input type="submit" name="calcular" value="Calcular">
             <input type="submit" name="salvar" value="Salvar">
             <input type="submit" name="pegar" value="Pegar">
+            <input type="submit" name="m" value="M">
             <input type="submit" name="limpar" value="Limpar Histórico">
         </form>
 
+
         <h2>Histórico de Operações</h2>
-        <?php
-        if (!empty($_SESSION['historico'])) {
-            echo "<ul>";
-            foreach ($_SESSION['historico'] as $calc) {
-                echo "<li>$calc</li>";
+        <textarea readonly rows="10" cols="40" style="resize: none;"><?php
+            if (!empty($_SESSION['historico'])) {
+                foreach ($_SESSION['historico'] as $calc) {
+                    echo "$calc\n";
+                }
+            } else {
+                echo "Nenhuma operação realizada ainda.";
             }
-            echo "</ul>";
-        } else {
-            echo "<p>Nenhuma operação realizada ainda.</p>";
-        }
-        ?>
+        ?></textarea>
     </div>
 </body>
 </html>
